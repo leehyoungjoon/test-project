0e29c20b104b926560d8ae773b579c7ca0b91a35
이형준 <gudwns970128@gmail.com>
2024-03-18 16:25:40 +0900

    WIP

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -36,17 +36,26 @@ const index = () => {
             onMouseLeave={handleMouseLeave}
             className={active ? "active" : ""}
           >
-            <NavItem href="#">서비스 안내</NavItem>
-            <NavItem href="#">서비스 신청</NavItem>
-            <NavItem href="#">이용 방법</NavItem>
-            <NavItem href="#">마이페이지</NavItem>
-            <NavItem href="#">고객센터</NavItem>
+            <div className="navItem">서비스 안내</div>
+            <div className="navItem">서비스 신청</div>
+            <div className="navItem">이용 방법</div>
+            <div className="navItem">마이페이지</div>
+            <div className="navItem">고객센터</div>
 
+            <div className="dropDownMenu">
+              <div className={active ? "active" : ""}>
+                <div className="dropDownItem">1</div>
+                <div className="dropDownItem">1</div>
+                <div className="dropDownItem">1</div>
+                <div className="dropDownItem">1</div>
+              </div>
+            </div>
+            {/* 
             <DropdownMenu className={active ? "active" : ""}>
               <DropdownItem href="#">Product 1</DropdownItem>
               <DropdownItem href="#">Product 2</DropdownItem>
               <DropdownItem href="#">Product 3</DropdownItem>
-            </DropdownMenu>
+            </DropdownMenu> */}
           </Nav>
         </div>
       </div>
@@ -58,7 +67,7 @@ const index = () => {
 export default index
 
 const NavBarWrapper = styled.nav`
-  background-color: black;
+  /* background-color: black; */
   height: 122px;
   display: flex;
   align-items: center;
@@ -76,19 +85,19 @@ const NavBarWrapper = styled.nav`
   }
 
   .bottomNavWrap {
-    background-color: green;
+    /* background-color: green; */
     width: 1072px;
     height: 88px;
 
     .bottomNav {
       display: flex;
       justify-content: space-between;
-      background-color: aqua;
+      /* background-color: aqua; */
 
       .dropMenu {
         display: flex;
         width: 880px;
-        background-color: blueviolet;
+        /* background-color: blueviolet; */
         padding-left: 81px;
         position: relative;
 
@@ -106,39 +115,62 @@ const NavBarWrapper = styled.nav`
 `
 
 const Nav = styled.nav`
-  position: fixed;
+  /* position: fixed;
   top: 0;
-  left: 0;
+  left: 0; */
   width: 100%;
   height: 50px;
   background-color: #222;
   color: white;
   z-index: 100;
-`
 
-const NavItem = styled.a`
-  display: inline-block;
-  padding: 10px;
-  color: white;
+  .navItem {
+    display: inline-block;
+    padding: 10px;
+    color: white;
 
-  &:hover {
-    background-color: #ccc;
+    &:hover {
+      background-color: #ccc;
+    }
+  }
+
+  .dropDownMenu {
+    position: absolute;
+    top: 50px;
+    left: 0;
+    width: 100%;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    display: none;
+
+    &.active {
+      display: block;
+    }
+
+    .dropDownItem {
+      padding: 10px;
+      color: #333;
+
+      &:hover {
+        background-color: #ccc;
+      }
+    }
   }
 `
 
-const DropdownMenu = styled.ul`
-  position: absolute;
-  top: 50px;
-  left: 0;
-  width: 100%;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  display: none;
+// const DropdownMenu = styled.ul`
+//   position: absolute;
+//   top: 50px;
+//   left: 0;
+//   width: 100%;
+//   background-color: #fff;
+//   border: 1px solid #ccc;
+//   display: none;
 
-  &.active {
-    display: block;
-  }
-`
+//   &.active {
+//     display: block;
+//   }
+// `
 
 const DropdownItem = styled.li`
   padding: 10px;
